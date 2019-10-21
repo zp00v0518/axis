f1182c95f000688c44ce252894a04fdf7a898c8d
Dmitriy <you@example.com>
2019-10-21 23:36:09 +0300

    draw base lines

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import Axis from '../utils/Axis';
+import Axis from "../utils/Axis";
 
 export default {
   name: "Chart",
@@ -13,7 +13,7 @@ export default {
     return {
       ctx: null,
       canvas: null,
-      axis: null,
+      axis: null
     };
   },
   methods: {
@@ -23,16 +23,35 @@ export default {
       this.canvas.setAttribute("width", styles.width + "px");
       this.canvas.setAttribute("height", styles.height + "px");
     },
+    draw() {
+      const {ctx, canvas, axis} = this;
+      ctx.clearRect(0,0,canvas.width, canvas.height)
+      axis.draw();
+    },
+    zoomingCanvas(event){
+      const {wheelDelta} = event;
+      const {axis} = this;
+      const zoom = axis.zoom;
+      const zoomStep = 1;
+      if (wheelDelta < 0 ){
+        axis.setZoom(zoom + zoomStep);
+      } else {
+        axis.setZoom(zoom  - zoomStep);
+      }
+      this.draw();
+    }
   },
   mounted() {
     this.canvas = this.$refs.canvas;
     this.ctx = this.canvas.getContext("2d");
     this.setSize();
     this.axis = new Axis(this.ctx);
-    this.axis.setOrign( this.canvas.width / 2,  this.canvas.height / 2 );
-    this.axis.setColor({fill: 'red'})
+    this.axis.setOrign(this.canvas.width / 2, this.canvas.height / 2);
+    // this.axis.setColor({ fill: "red" });
     this.axis.setCenterSize(3);
-    this.axis.draw();
+    this.draw();
+    window.addEventListener("resize", this.draw);
+    this.canvas.addEventListener("wheel", this.zoomingCanvas);
   }
 };
 </script>
--- src/utils/Axis.js
+++ src/utils/Axis.js
@@ -1,30 +1,87 @@
 import Color from './Color';
 class Axis extends Color {
-  constructor(ctx, x = 0, y = 0, options) {
+  constructor(ctx, x = 0, y = 0) {
     super(ctx);
     if (!ctx) {
       console.log('CTX is requierd');
       return;
     }
     this.ctx = ctx;
+    this.canvas = this.ctx.canvas;
     this.x = x;
     this.y = y;
     this.size = 1;
+    this.zoom = 5;
+    this.baseStep = 5;
+    this.curStep = this.baseStep * this.zoom;
   }
   setOrign(x, y) {
     this.x = x;
     this.y = y;
+  }
+  setZoom(num) {
+    this.zoom = num <= 0 ? 1 : num;
+    this.curStep = this.baseStep * this.zoom;
+  }
+  setStep(num) {
+    this.baseStep = num;
   }
   setCenterSize(size) {
     this.size = size;
   }
   draw() {
     this.drawCenter();
+    this.drawBaseLines();
   }
   drawCenter() {
     const { ctx, x, y, size } = this;
     ctx.beginPath();
     ctx.arc(x, y, size, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+  }
+  drawBaseLines() {
+    this.drawLines('x');
+    this.drawLines('y');
+    this.drawStepOnLine('x');
+    this.drawStepOnLine('y');
+  }
+  drawLines(way) {
+    const { ctx, x, y } = this;
+    ctx.beginPath();
+    if (way === 'x') {
+      ctx.moveTo(0, y);
+      ctx.lineTo(ctx.canvas.width, y);
+    } else if (way === 'y') {
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, ctx.canvas.height);
+    }
+    ctx.stroke();
+    ctx.closePath();
+  }
+  drawStepOnLine(way) {
+    const { ctx, x, y, curStep, size } = this;
+    let count = 1;
+    let point;
+    const general = [size / 1.5, 0, 2 * Math.PI];
+    ctx.beginPath();
+    if (way === 'y') {
+      point = y - curStep;
+      while (point > 0) {
+        ctx.arc(x, point, ...general);
+        ctx.arc(x, point + count * curStep * 2, ...general);
+        point -= curStep;
+        ++count;
+      }
+    } else if (way === 'x') {
+      point = x - curStep;
+      while (point > 0) {
+        ctx.arc(point, y, ...general);
+        ctx.arc(point + count * curStep * 2, y, ...general);
+        point -= curStep;
+        ++count;
+      }
+    }
     ctx.fill();
     ctx.closePath();
   }
